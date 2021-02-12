1cf49c56273148c45faa9be9d0f78134349ce0cd
Victor <victor_993@msn.com>
2021-02-12 14:44:15 -0800

    added enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 test('renders learn react link', () => {
   render(<App />);
