08a9e6b6448f801164a5e7b99109d04e3487c81e
Victor <victor_993@msn.com>
2021-02-13 10:55:20 -0800

    added bootstrap for basic styling

--- src/Congrats.js
+++ src/Congrats.js
@@ -5,7 +5,7 @@ const Congrats = (props) => {
   return (
     <div>
       {props.success ? (
-        <div data-test='component-congrats'>
+        <div data-test='component-congrats' className='alert alert-success'>
           <span data-test='congrats-message'>
             Congratulations! You guessed the word!
           </span>
